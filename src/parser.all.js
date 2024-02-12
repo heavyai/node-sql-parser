869ad889a5f6dbdd8ad34018022a54e2268cd802
Joseph O'Hallaron <joe.ohallaron@heavy.ai>
2024-02-12 09:18:32 -0700

    Includes heavydb parser in all

--- src/parser.all.js
+++ src/parser.all.js
@@ -9,6 +9,7 @@ import { parse as transactsql } from '../pegjs/transactsql.pegjs'
 import { parse as flinksql } from '../pegjs/flinksql.pegjs'
 import { parse as snowflake } from '../pegjs/snowflake.pegjs'
 import { parse as noql } from '../pegjs/noql.pegjs'
+import { parse as heavydb } from '../pegjs/heavydb.pegjs'
 
 export default {
   bigquery,
@@ -22,4 +23,5 @@ export default {
   transactsql,
   flinksql,
   noql,
+  heavydb,
 }
