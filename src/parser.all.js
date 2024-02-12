21b0753f883585a3ead02407bb7e1101d00e4670
Joseph O'Hallaron <joe.ohallaron@heavy.ai>
2024-02-12 09:18:32 -0700

    Add heavydb to parser.all.js

--- src/parser.all.js
+++ src/parser.all.js
@@ -2,24 +2,26 @@ import { parse as athena } from '../pegjs/athena.pegjs'
 import { parse as bigquery } from '../pegjs/bigquery.pegjs'
 import { parse as db2 } from '../pegjs/db2.pegjs'
 import { parse as flinksql } from '../pegjs/flinksql.pegjs'
+import { parse as heavydb } from '../pegjs/heavydb.pegjs'
 import { parse as hive } from '../pegjs/hive.pegjs'
-import { parse as mysql } from '../pegjs/mysql.pegjs'
 import { parse as mariadb } from '../pegjs/mariadb.pegjs'
+import { parse as mysql } from '../pegjs/mysql.pegjs'
 import { parse as noql } from '../pegjs/noql.pegjs'
 import { parse as postgresql } from '../pegjs/postgresql.pegjs'
 import { parse as redshift } from '../pegjs/redshift.pegjs'
+import { parse as snowflake } from '../pegjs/snowflake.pegjs'
 import { parse as sqlite } from '../pegjs/sqlite.pegjs'
 import { parse as transactsql } from '../pegjs/transactsql.pegjs'
-import { parse as snowflake } from '../pegjs/snowflake.pegjs'
 
 export default {
   athena,
   bigquery,
   db2,
   flinksql,
+  heavydb,
   hive,
-  mysql,
   mariadb,
+  mysql,
   noql,
   postgresql,
   redshift,
